6512393c29d62846e357e89b8c89046f67751e6e
thanhnamzzz <ksthanhnam1995@gmail.com>
2022-08-04 22:21:30 +0700

    update case

--- TETRIS_GAME.js
+++ TETRIS_GAME.js
@@ -14,6 +14,25 @@ class frame {
         this.grid = this.whiteBoard();
     }
     whiteBoard() {
-        return Array.from()
+        return Array(rows).fill(Array(columns).fill(white_color_id));
+    }
+    drawCell(xAxis, yAxis, colorId) {
+        this.board.fillStyle = color_box[colorId] || color_box[white_color_id];
+        this.board.fillRect(xAxis*block_size, yAxis*block_size, block_size, block_size);
+        this.board.strokeStyle = "#000000";
+        this.board.strokeRect(xAxis*block_size, yAxis*block_size, block_size, block_size);
+        this.board.strokeStyle = "#FFFFFF";
+        this.board.strokeRect(xAxis*block_size+2, yAxis*block_size+2, block_size-4, block_size-4);
+    }
+    drawFrame() {
+        for (let row = 0; row < this.grid.length; row++){
+            for (let col = 0; col < this.grid[0].length; col++) {
+                this.drawCell(col, row, white_color_id);
+            }
+        }
     }
 }
+test = new frame(board);
+test.drawFrame();
+test.drawCell(1,1,5);
+console.table(test.grid);
